ce1c22144a97924e6cebbf972ef62138a8cbfd2f
Miguel González <miguel.gc@me.com>
2025-07-17 00:02:08 +0200

    feat: added Prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,34 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, fontProviders } from 'astro/config';
-import rehypeKatex from 'rehype-katex';
-import remarkMath from 'remark-math';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   experimental: {
     fonts: [
-        {
-            provider: fontProviders.google(),
-            name: 'Inter',
-            cssVariable: '--font-inter',
-            weights: ['100 900'],
-        },
-        {
-            provider: fontProviders.google(),
-            name: 'Playfair Display',
-            cssVariable: '--font-playfair',
-            weights: ['400 900'],
-            styles: ['italic', 'normal'],
-        },
+      {
+        provider: fontProviders.google(),
+        name: "Inter",
+        cssVariable: "--font-inter",
+        weights: ["100 900"],
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Playfair Display",
+        cssVariable: "--font-playfair",
+        weights: ["400 900"],
+        styles: ["italic", "normal"],
+      },
     ],
-},
+  },
   markdown: {
     syntaxHighlight: "prism",
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex]
-  }
+    rehypePlugins: [rehypeKatex],
+  },
 });
