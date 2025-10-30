fb704a725599e0ec377e8425a467a665dfa8c740
Miguel González <miguel.gc@me.com>
2025-10-30 12:52:07 +0100

    feat: add course info

---
+++ src/pages/courses/uax-2025-blockchain.md
@@ -0,0 +1,6 @@
+---
+title: "Blockchain"
+description: "Universidad Alfonso X el Sabio"
+layout: "../../layouts/MarkdownLayout.astro"
+---
+*Universidad Alfonso X el Sabio*
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    typography: {
+      default: {
+        css: {
+          'code::before': {
+            content: '""',
+          },
+          'code::after': {
+            content: '""',
+          }
+          // ...
+        },
+      },
+    },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require('@tailwindcss/typography'),
+    // ...
+  ],
 };
